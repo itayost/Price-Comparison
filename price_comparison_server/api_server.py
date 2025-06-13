fdda6148694f9a4fa5ef09f1830e2a0724609073
itayost <itayost1@gmail.com>
2025-06-13 20:41:53 +0300

    pass

--- price_comparison_server/api_server.py
+++ price_comparison_server/api_server.py
@@ -64,6 +64,7 @@ def start_scheduled_scraping():
 @app.on_event("startup")
 def startup_event():
     #start_scheduled_scraping()
+    pass
 
 # Run the API server with:
 # uvicorn api_server:app --host 0.0.0.0 --reload
